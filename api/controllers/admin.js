5e72c2846b66ec07808bac112051f0b78674aa77
rajat <rajat110898@gmail.com>
2019-11-21 01:38:14 +0530

    some admin routes added

--- api/controllers/admin.js
+++ api/controllers/admin.js
@@ -6,21 +6,21 @@ const User = require('../models/user');
 exports.admin_listall = (req,res,next)=>{
     if(req.userData.isAdmin != '1'){
         res.status(401).json({
-            message: 'Auth Failed'
+            message: 'Auth Failed Not an admin'
         })
     }else{
         User.find()
-        .select('name email booking status')
+        .select('name email booking _id')
         .exec()
         .then(docs=>{
             const response = {
                 count: docs.length,
                 users: docs.map(doc=>{
                     return {
+                        _id: doc._id,
                         name: doc.name,
                         email: doc.email,
                         booking: doc.bookings,
-                        _id: doc._id,
                         request: {
                             type: 'GET'
                         }
@@ -37,3 +37,66 @@ exports.admin_listall = (req,res,next)=>{
         })
     }
 }
+
+exports.users_get_user = (req,res,next)=>{
+    if(req.userData.isAdmin!=1){
+        res.status(401).json({
+            message: 'Auth Failed'
+        })
+    }else{
+        const id = req.params.userId;
+        User.findById(id)
+        .select('name email _id')
+        .exec()
+        .then(doc=>{
+            if(doc){
+                res.status(200).json({
+                    user: doc,
+                    request: {
+                        type: 'GET',
+                        url: "http://localhost:4000/admin/"
+                    }
+                });
+            }else{
+                res.status(404).json({
+                    message: 'No valid entry'
+                });
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        })
+    }
+}
+
+exports.users_delete_user = (req,res,next)=>{
+    if(req.userData.isAdmin!=1){
+        res.status(401).json({
+            message: 'Auth Failed'
+        })
+    }else{
+        const id = req.params.userId;
+        User.remove( { _id: id})
+        .exec()
+        .then(result=>{
+            res.status(200).json({
+                message: 'Product Deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:4000/user/signup',
+                    body: { name: 'String', email: 'String', password: 'String'}
+                }
+            });
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        })
+    }
+}
+
--- api/routes/admin.js
+++ api/routes/admin.js
@@ -5,6 +5,8 @@ const AdminController = require('../controllers/admin');
 const checkAuth = require('../middleware/check-auth');
 
 router.get('',checkAuth ,AdminController.admin_listall);
+router.get('/:userId',checkAuth ,AdminController.users_get_user);
+router.delete('/:userId',checkAuth ,AdminController.users_delete_user);
 
 module.exports = router;
 
